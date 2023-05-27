5b1d89492c65f2cc50f6b0208175e535756783ed
shubham2o <shubhamsharma.ha@gmail.com>
2023-05-28 00:22:41 +0530

    index.html, style.css, app.js linked

---
+++ app.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById('header'));
+
+const parent =
+    React.createElement(
+        "div", {
+            id: 'parent',
+            style: {
+                backgroundColor: "red",
+                color: "white"
+            }
+        }, [React.createElement(
+            "div", {
+                id: 'child1'
+            }, [React.createElement("h1", {}, "This is Namaste React 🚀")], [React.createElement("h2", {}, "By Shubham Sharma")]
+        )], [React.createElement(
+            "div", {
+                id: 'child2'
+            }, [React.createElement("h1", {}, "I am h1 tag")], [React.createElement("h2", {}, "I am h2 tag")]
+        )]
+    );
+
+root.render(parent);
+
+const heading = React.createElement('h1', {
+        id: 'heading',
+        xyz: "abc"
+    },
+    "Hello World from React!"
+);
